1a7db1b494020b48370e3695e18d49b8ace9b91f
OLEKSANDR <68386153+Alex21777now@users.noreply.github.com>
2021-05-05 17:53:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var express = require ('express');
+const express = require ('express')
 
 var bodyParser = require('body-parser');
 
-var app = express();
+const app = express()
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+const PORT = process.env.PORT || 80
 
 app.set('view engine', 'ejs');
 
@@ -30,4 +32,4 @@ app.get('/news/:id', function(req, res) {
   res.render('news', {newsId: req.params.id, newParam: 234, obj: obj});
 });
 
-app.listen(3000);
+app.listen(PORT, () => {console.log('Server has been started...')})
